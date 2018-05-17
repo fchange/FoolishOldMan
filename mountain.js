ae617ba11be3b9a4c606ab88a3086d150b098986
方程 <fchangenow@163.com>
2018-05-17 18:02:59 +0800

    :hankey: bad code

--- mountain.js
+++ mountain.js
@@ -1,90 +1,98 @@
 'use strict';
 
 var MGame = function() {
-    LocalContractStorage.defineProperty(this, "MountainWeight");
-    LocalContractStorage.defineProperty(this, "currentControlName");
-    LocalContractStorage.defineProperty(this, "currentControlTime");
-    LocalContractStorage.defineProperty(this, "GameOver");
+    LocalContractStorage.defineProperty(this, {
+        "MountainWeight" : 100,
+        "currentControlName" : '',
+        "currentControlTime" : 0,
+        "currentState" : '',  // ON OVER
+        "currentGameNum" : 0
+    });
+
     LocalContractStorage.defineMapProperty(this, "UserNameMap");
-    LocalContractStorage.defineMapProperty(this, "NameScoreMap");//Todo: 历史纪录Map
-    LocalContractStorage.defineProperty(this, "NamekeyList");
+    LocalContractStorage.defineMapProperty(this, "NameUserMap");
+    LocalContractStorage.defineMapProperty(this, "NameScoreMap");
+    LocalContractStorage.defineMapProperty(this, "TimeNameList");//每次玩家 {1 ==> []} includes
 };
 
 MGame.prototype = {
 
     init: function() {
-        this.MountainWeight = 100
-        this.currentControlName = ''
-        this.currentControlTime = 0
-        this.GameOver =false
+        this.currentState = 'ON',
+        this.TimeNameList.set(this.currentGameNum, [])
     },
 
     dug: function() {
         if(this.UserNameMap.get(Blockchain.transaction.from)) {
             return 'Please set a name' 
         } else if(this.currentControlName != '') {
-            return this.currentControlName + ' is working'
+            return this.currentControlName + ' is DUGing'
         } else{
             this.currentControlName = this.UserNameMap.get(Blockchain.transaction.from) 
-            this.currentControlTime = Date.now()
+            this.currentControlTime = Blockchain.block.timestamp
             return 'Fool Old Man is Working Now'
         }
     },
 
     _oldManWorkDone: function() {
         this.MountainWeight -= 1
-        const score = this.NameScoreMap.get(this.currentControlName)
-        this.NameScoreMap.set(this.currentControlName, score+1)
+
+        const score = this.NameScoreMap.get(`${this.currentControlTime}_${this.currentControlName}`) || 0
+        this.NameScoreMap.set(`${this.currentControlTime}_${this.currentControlName}`, score+1)
+
+        const gamers = this.TimeNameList.get(this.currentGameNum) || []
+        if(!gamers.includes(this.currentControlName)) {
+            this.TimeNameList.set(this.currentGameNum, gamers.concat(this.currentControlName))
+        }
+
         this.currentControlName = ''
+        this.currentControlTime = 0
         this.MountainWeight == 0 && _gameOver()
     },
 
     _gameOver: function() {
-        this.GameOver = true
         this.currentControlTime = Date.now()
+        this.currentState = 'OVER'
     },
 
-    _reset: function() {
-        this.GameOver = false
-
+    _resetGame: function() {
         this.MountainWeight = 100
-        this.currentControlName = ''
         this.currentControlTime = 0
-        this.GameOver =false
-    }
+        this.currentState = 'ON'
+        this.currentGameNum += 1
+    },
 
     getState: function() {
-        if(Date.now() - 2000 > this.currentControlTime && this.currentControlName != '') {
+        if(this.currentState == 'ON'
+                && this.currentControlName != '' 
+                && Blockchain.block.timestamp - 2000 > this.currentControlTime ) {
             this._oldManWorkDone()
-            this.currentControlTime = 0
-        } else if(this.GameOver && Date.now() - 200000 < this.currentControlTime) {
-            return {
-                GameOver: this.GameOver,
-                currentControlTime: this.currentControlTime,
-                NameScoreMap: NameScoreMap,
-            }
-        } else if(this.GameOver && Date.now() - 200000 >= this.currentControlTime) {
-            this._reset()
-        }
+        } else if(Blockchain.block.timestamp - 200000 > this.currentControlTime
+                && this.currentState == 'OVER' ) {
+            this._resetGame()
+        } 
         return {
             MountainWeight: this.MountainWeight,
             currentControlName: this.currentControlName,
             currentControlTime: this.currentControlTime,
+            currentState: this.currentState,
             NameScoreMap: this.NameScoreMap,
+            TimeNameList: this.TimeNameList
         }
     },
 
     canIUseName: function(name) {
-        return isNaN(this.UserNameMap.get(name))
+        return isNaN(this.NameUserMap.get(name))
     },
 
     setName: function(name) {
-        if(isNaN(this.UserNameMap.get(name))) {
+        if(isNaN(this.NameUserMap.get(name))) {
+            this.NameUserMap.set(name, Blockchain.transaction.from)
             this.UserNameMap.set(Blockchain.transaction.from, name)
             this.NameScoreMap.set(name, 0)
             return 'Success'
         } else {
-            return 'Name has been used'
+            throw new Error('Name has been used')
         }
     }
 };
